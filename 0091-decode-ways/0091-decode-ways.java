70218cc858bcfcbef216f5474c3606146ec60c39
VishalKumar3153 <115474976+VishalKumar3153@users.noreply.github.com>
2024-07-25 11:41:23 +0530

    Time: 1 ms (79.9%), Space: 41.3 MB (89.28%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int numDecodings(String s) {
+        int dp[]=new int[100];
+        Arrays.fill(dp,-1);
+        return decode(0,s,dp);
+    }
+    public int decode(int i,String s,int[] dp)
+    {
+        if(i>=s.length())
+        {
+            return 1;
+        }
+        if(dp[i]!=-1)
+        {
+            return dp[i];
+        }
+        int ans=0;
+        int op1=s.charAt(i)-'0',op2=0;
+        if(i<=s.length()-2)
+        {
+            op2=op1*10+s.charAt(i+1)-'0';
+        }
+        if(op1>0)
+        {
+           ans+= decode(i+1,s,dp);
+        }
+        if(op1>0 && (op2>0 && op2<=26))
+        {
+            ans+=decode(i+2,s,dp);
+        }
+        return dp[i]=ans;
+    }
+}
